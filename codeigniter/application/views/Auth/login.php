dff2578802183cdbfe783ff9a829d223820f6e20
KietVuB19 <kvu0408@gmail.com>
2024-06-18 11:51:52 +0700

    Update login.php

--- codeigniter/application/views/Auth/login.php
+++ codeigniter/application/views/Auth/login.php
@@ -2,9 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta name="author" content="Muhamad Nauval Azhar">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta name="description" content="This is a login page template based on Bootstrap 5">
 	<title>Login page</title>
 	<link rel="stylesheet" href="<?php echo site_url() . 'Css_fold/all.css'; ?>">
 	<link rel="stylesheet" href="<?php echo site_url() . 'Css_fold/toast/toast.min.css'; ?>">
